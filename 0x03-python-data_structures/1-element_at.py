d55f782323984e7a0137a512a2fbddb95664e5e6
Breezekid <calebbaraka79@gmail.com>
2023-11-05 21:32:27 +0300

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-element_at.py
+# calebbaraka79@gmail.com
+
+def element_at(my_list, idx):
+    """Retrieve an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
