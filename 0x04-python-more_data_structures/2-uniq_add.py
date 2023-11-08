ec1708bec2b70b563e34899aeb98b9a71e327bd4
Breezekid <calebbaraka79@gmail.com>
2023-11-08 22:31:32 +0300

    5-number_key

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#calebbaraka79@gmail.com
+
+def uniq_add(my_list=[]):
+    """Add all unique integers in a list."""
+    result = 0
+    for x in result(my_list):
+        result += x
+    return (result)
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#calebbaraka79@gmail.com
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
