6c6f1f44311387559502b7ca947ed8fcef3d1877
Breezekid <calebbaraka79@gmail.com>
2023-11-08 22:49:11 +0300

    8-simple_delete.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#calebabaraka79@gmail.com
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds key/value in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
