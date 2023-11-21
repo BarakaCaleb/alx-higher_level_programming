7147098f851a200e45695855dc58d71e19894150
Breezekid <calebbaraka79@gmail.com>
2023-11-22 02:54:21 +0300

    fixed more errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,36 +1,33 @@
-#!/usr/bin/python3
+"""Define a class Square."""
 
-"""Define a class sqaure."""
 
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0. position=(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
-
         self.size = size
         self.position = position
 
-        @property
-        def size(self):
-            """Get/set the current size of the square."""
-            return (self.__size)
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-
-            @property
+    @property
     def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
