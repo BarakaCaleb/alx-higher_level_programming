c40e32e89e2bd34b16612d373995828c0a7fbbde
Breezekid <calebbaraka79@gmail.com>
2023-11-22 01:50:22 +0300

    tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+
+class square:
+    """Represents a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+class square:
+    ""Represents a square"""
+
+    def __init__(self,size):
+        """Initialize a new sqaure.
+
+        Args:
+            size(int): The size of the new square.
+        """
+        self.__size = size
