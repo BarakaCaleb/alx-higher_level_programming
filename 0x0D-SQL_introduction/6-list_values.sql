03a462fccbe32fe1145d4844dfd7f5889403441a
Breezekid <calebbaraka79@gmail.com>
2024-02-14 21:24:28 +0300

    changes

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
---lists all rows of the table from the database hbtn_0c_0 in your MYSQL server
--- Database name will be passed as an argument in mysql command
+-- lists all rows of the table
+-- database name will be passed as an argument
 SELECT *
 FROM first_table;
