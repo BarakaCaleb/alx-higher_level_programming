eea349cd501ed6a7d8bacdda3892aa956cba10f3
Breezekid <calebbaraka79@gmail.com>
2024-02-14 21:34:27 +0300

    Task 8

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Inserts a new row in the table first_table in your MYSQL server
+-- The database name will be passed as an argument in the mysql command 
+INSERT INTO first_table
+VALUES(89, 'Best School');
