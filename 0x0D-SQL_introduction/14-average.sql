eea6ded59b2a73d3310121eabf978e3eb2620eb8
Breezekid <calebbaraka79@gmail.com>
2024-02-15 13:36:20 +0300

    Task 15

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the average of all records in the table of the database
+-- Database name will be passed as an argument of the mysql command
+SELECT(AVG) score as Average
+FROM second_table;
