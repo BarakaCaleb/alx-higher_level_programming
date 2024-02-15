e9bed1ec1707e03d1782e13e1650d7888058c42e
Breezekid <calebbaraka79@gmail.com>
2024-02-15 14:09:20 +0300

    task 101

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Convert a database to UTF8 in your MYSQL database
+-- Convert the following 
+-- Database hbtn_0c_0
+-- Table first_table
+-- Field name in first_table
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
