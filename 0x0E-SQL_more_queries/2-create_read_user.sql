d2563688b3b6e6a40040838ee2d18c03994c26fd
Breezekid <calebbaraka79@gmail.com>
2024-02-16 15:25:38 +0300

    changes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- Creates a database hbtn_0d_2 and a user
+-- create a database, a user abd gives him the database PRIVILEGES
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 ON hbtn_0d_2.*
-TO 'user_od_2'@'localhost';
+TO 'user_0d_2'@'localhost';
