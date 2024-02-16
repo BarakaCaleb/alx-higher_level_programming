25a6d5e94efd76d20e2b78f72dfc294325cf6e0c
Breezekid <calebbaraka79@gmail.com>
2024-02-16 15:49:44 +0300

    Task 9

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- lists all cities of California
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California');
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities in the DATABASE
+SELECT c.id, c.name, s.name
+FROM cities c
+INNER JOIN states s ON c.state_id = s.id
+ORDER BY c.id ASC;
