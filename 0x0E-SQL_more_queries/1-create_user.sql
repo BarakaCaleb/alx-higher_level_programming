775cef32437bca08ff2e39e67f808b5fc5bd6b30
Breezekid <calebbaraka79@gmail.com>
2024-02-16 15:17:37 +0300

    changes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Creates a user in the mysql database
-CREATE IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PREVILIGES ON *.* TO 'user_0d_1'@'localhost';
+-- creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
